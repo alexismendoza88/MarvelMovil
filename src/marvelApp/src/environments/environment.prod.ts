5dea68613e6336212dbc8bbf536992533ea0a906
alexismendoza88 <amendoza.dym@gmail.com>
2025-02-26 11:14:21 -0500

    Update environment.prod.ts

--- src/marvelApp/src/environments/environment.prod.ts
+++ src/marvelApp/src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   marvel: {
-    publicKey: '3182770fe28b8ad95286c14c7d853d14',
-    privateKey: '63e3252d8c90ee3734df62582c0527326704ce14', // if needed for generating hash
+    publicKey: '',
+    privateKey: '', // if needed for generating hash
     baseUrl: 'https://gateway.marvel.com/v1/public'
   }
 
